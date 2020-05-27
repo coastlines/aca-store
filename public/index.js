25943408b0f5b42d152e39bb815ee7ddc281a5ab
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-05-26 20:27:16 -0500

    product + product details + css

--- public/index.js
+++ public/index.js
@@ -1,2 +1,38 @@
-//stuff
-//more stuff
+'use strict';
+
+window.onload = function() {
+  getProducts()
+  console.log('hi')
+}
+
+const getProducts = () => {
+      fetch('https://my-json-server.typicode.com/jubs16/Products/Products')
+    .then(response => { 
+      return response.json()
+    })
+    .then(data => {
+      data.forEach(productDiv);
+    })
+}
+
+let productDiv= function(data) {
+  let section = document.getElementById("product-container");
+
+  let div = document.createElement("div");
+  let img = document.createElement("img");
+  let p = document.createElement("p");
+  let button = document.createElement("button");
+  let name = data.name.toLowerCase();
+
+  section.classList.add("flex");
+  div.classList.add("product");
+  div.classList.add("flex-column");
+  img.src = data.imgUrl;
+  p.innerHTML = `<strong>${name}</strong> $${data.price}`;
+  button.innerHTML = "Details";
+  
+  div.appendChild(img);
+  div.appendChild(p);
+  div.appendChild(button);
+  section.appendChild(div);
+}
